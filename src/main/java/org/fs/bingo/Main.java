4ee4627ccee683ad2dc6e02c822ac6989655cd37
fsablaon <fsablaon@gmail.com>
2022-04-17 21:25:20 -0700

    Add support quit (Q) and enter key input.
    1. Quit will exit program
    2. Enter will select program default

--- src/main/java/org/fs/bingo/Main.java
+++ src/main/java/org/fs/bingo/Main.java
@@ -4,54 +4,68 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static final boolean DEBUG = false;
+	public static final boolean DEBUG = true;
+	private static final Scanner scanner = new Scanner(System.in);
+	private static final String QUIT_CHAR = "Q";
 
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		print("**** Lets Play Housie *****");
-		print("");
-		print("Note: - Press 'Q'; to quit any time.");
+		printHeader();
 
-		print(">>Enter the number range(1-n): ");
-		int range = scanner.nextInt();
+		Parameters.Builder builder = new Parameters.Builder();
 
-		print(">>Enter Number of players playing the game: ");
-		int players = scanner.nextInt();
+		String range = collectInput(">>Enter the number range(1-n): ");
+		if (!range.isEmpty())
+			builder.withRange(Integer.parseInt(range));
 
-		print(">>Enter Ticket Size : Default to 3X10");
-		final String s1 = scanner.next(); //Use pattern?
-		String[] s = s1.split("X"); //TODO: add validation
-		int ticketRow = Integer.parseInt(s[0]);
-		int ticketCol = Integer.parseInt(s[1]);
+		String players = collectInput(">>Enter Number of players playing the game: ");
+		if (!players.isEmpty())
+			builder.withPlayers(Integer.parseInt(players));
 
-		print(">>Enter numbers per row. Default to 5");
-		int numPerRow = scanner.nextInt();
+		String dimensions = collectInput(">>Enter Ticket Size : Default to 3X10");
+		if (!dimensions.isEmpty()){
+			String[] s = dimensions.split("X"); //TODO: add validation
+			builder.withTicketRow(Integer.parseInt(s[0]));
+			builder.withTicketCol(Integer.parseInt(s[1]));
+		}
 
-		Parameters params = new Parameters.Builder()
-						.withRange(range)
-						.withPlayers(players)
-						.withTicketRow(ticketRow)
-						.withTicketCol(ticketCol)
-						.withNumPerRow(numPerRow)
-						.build();
+		String numPerRow = collectInput(">>Enter numbers per row. Default to 5");
+		if (!numPerRow.isEmpty())
+			builder.withNumPerRow(Integer.parseInt(numPerRow));
+
+		Parameters params = builder.build();
 
 		if (DEBUG)
 			print(params.toString());
 
-		print("***Ticket Created Successfully ****");
-
 		BingoFactory factory = new BingoFactory(params);
 		Bingo bingo = factory.create();
 
-		print(">> Press 'N' to generate next number");
+		print("***Ticket Created Successfully ****");
 
+		print(">> Press 'N' to generate next number");
 		boolean gameOver = false;
 		while(!gameOver){
-			String input = scanner.next();
+			String input = collectInput("");
 			if (input.equalsIgnoreCase("n"))
 				gameOver = bingo.play();
 		}
+	}
 
+	private static String collectInput(String message) {
+		if (!message.isEmpty())
+			print(message);
+
+		String input = scanner.nextLine();
+		if (QUIT_CHAR.equalsIgnoreCase(input))
+			System.exit(0);
+
+		return input;
+	}
+
+	private static void printHeader() {
+		print("**** Lets Play Housie *****");
+		print("");
+		print("Note: - Press 'Q'; to quit any time.");
 	}
 
 	/**
